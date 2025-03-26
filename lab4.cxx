474a1b8843faf7130c42b00f214f0766cdfe4d24
Buster Daemon <bugsbunnygdi@pm.me>
2025-03-26 21:37:29 +0300

    Сделана лаба 5

--- lab4.cxx
+++ lab4.cxx
@@ -2,9 +2,19 @@
 #include <algorithm>
 #include <iostream>
 #include <memory>
+#include <new>
+#include <stddef.h>
+#include <stdexcept>
+#include <stdint.h>
 #include <stdlib.h>
 #include <unordered_set>
 #include <utility>
+
+#define _BAD_MEMORY_TEXT "Ошибка выделения памяти: "
+#define _BAD_ARGUMENT_TEXT "Аргумент не может ссылаться на NULL\n"
+#define _BAD_ARG_OR_SIZE_TEXT                                                  \
+  "Аргумент не должен ссылаться на NULL или иметь размер меньше 1.\n"
+#define _BAD_OUT_OF_INDEX_ARRAY "Выход за пределы массива\n"
 
 template <typename T> set<T>::set() {
   this->count = 0;
@@ -12,20 +22,28 @@ template <typename T> set<T>::set() {
 }
 
 template <typename T> set<T>::set(T elem) {
-  this->count = 1;
-  std::unique_ptr<T[]> set_elem(new T[this->count]);
-  set_elem[this->count - 1] = std::move(elem);
-  this->elements = std::move(set_elem);
+  try {
+    this->count = 1;
+    std::unique_ptr<T[]> set_elem(new T[this->count]);
+    set_elem[this->count - 1] = std::move(elem);
+    this->elements = std::move(set_elem);
+  } catch (const std::bad_alloc &e) {
+    std::cerr << _BAD_MEMORY_TEXT << e.what() << "\n";
+  }
 }
 
 template <typename T> set<T>::~set() {}
 
 template <typename T> set<T>::set(set &&other) noexcept {
+  if (other.elements == nullptr)
+    throw std::invalid_argument(_BAD_ARGUMENT_TEXT);
   this->elements = std::move(other.elements);
   this->count = other.count;
 }
 
 template <typename T> set<T> &set<T>::operator=(set<T> &&other) noexcept {
+  if (other.elements == nullptr)
+    throw std::invalid_argument(_BAD_ARGUMENT_TEXT);
   if (this != &other) {
     elements = std::move(other.elements);
     count = other.count;
@@ -34,25 +52,35 @@ template <typename T> set<T> &set<T>::operator=(set<T> &&other) noexcept {
 }
 
 template <typename T> void set<T>::add_elem(T elem) {
-  size_t upd_size = this->count + 1;
-  std::unique_ptr<T[]> upd_arr(new T[upd_size]);
+  try {
+    size_t upd_size = this->count + 1;
+    std::unique_ptr<T[]> upd_arr(new T[upd_size]);
 
-  for (size_t i = 0; i < this->count; i++) {
-    upd_arr[i] = std::move(this->elements[i]);
+    for (size_t i = 0; i < this->count; i++) {
+      upd_arr[i] = std::move(this->elements[i]);
+      if (i >= this->count) {
+        throw std::overflow_error(_BAD_OUT_OF_INDEX_ARRAY);
+      }
+    }
+
+    upd_arr[this->count] = std::move(elem);
+
+    this->elements = std::move(upd_arr);
+    this->count = upd_size;
+  } catch (const std::bad_alloc &e) {
+    std::cerr << _BAD_MEMORY_TEXT << e.what() << "\n";
   }
-
-  upd_arr[this->count] = std::move(elem);
-
-  this->elements = std::move(upd_arr);
-  this->count = upd_size;
 }
 
 template <typename T> void set<T>::print() {
   if (this->elements == nullptr || this->count < 1) {
-    return;
+    throw std::invalid_argument(_BAD_ARG_OR_SIZE_TEXT);
   }
 
   for (size_t i = 0; i < this->count; i++) {
+    if (i >= this->count)
+      throw std::overflow_error(_BAD_OUT_OF_INDEX_ARRAY);
+
     std::cout << this->elements[i];
     if (i < this->count - 1)
       std::cout << ", ";
@@ -67,6 +95,9 @@ template <typename T> set<T> set<T>::operator-(const T &elem) {
 
   uint32_t occurences = 0;
   for (size_t i = 0; i < this->count; i++) {
+    if (i >= this->count)
+      throw std::overflow_error(_BAD_OUT_OF_INDEX_ARRAY);
+
     if (this->elements[i] == elem) {
       occurences = occurences + 1;
     }
@@ -78,16 +109,23 @@ template <typename T> set<T> set<T>::operator-(const T &elem) {
   }
 
   set<T> upd_set;
+  try {
+    size_t upd_size = this->count - occurences;
+    std::unique_ptr<T[]> upd_arr(new T[upd_size]);
+    for (size_t i = 0; i < upd_size; i++) {
+      if (i >= upd_size)
+        throw std::overflow_error(_BAD_OUT_OF_INDEX_ARRAY);
 
-  size_t upd_size = this->count - occurences;
-  std::unique_ptr<T[]> upd_arr(new T[upd_size]);
-  for (size_t i = 0; i < upd_size; i++) {
-    if (this->elements[i] != elem)
-      upd_arr[i] = std::move(this->elements[i]);
+      if (this->elements[i] != elem)
+        upd_arr[i] = std::move(this->elements[i]);
+    }
+
+    upd_set.count = upd_size;
+    upd_set.elements = std::move(upd_arr);
+  } catch (const std::bad_alloc &e) {
+    std::cerr << _BAD_MEMORY_TEXT << e.what() << "\n";
   }
 
-  upd_set.count = upd_size;
-  upd_set.elements = std::move(upd_arr);
   return std::move(upd_set);
 }
 
@@ -100,6 +138,9 @@ template <typename T> bool set<T>::operator>(set<T> &other_set) const {
     return true;
 
   for (size_t i = 0; i < main_src.count; i++) {
+    if (i >= main_src.count)
+      throw std::overflow_error(_BAD_OUT_OF_INDEX_ARRAY);
+
     main_set.insert(main_src.elements[i]);
   }
 
@@ -117,9 +158,13 @@ template <typename T> bool set<T>::operator!=(set<T> &other_set) const {
   std::unordered_set<T> main_set;
   for (size_t i = 0; i < this->count; i++) {
     main_set.insert(this->elements[i]);
+    if (i >= this->count)
+      throw std::overflow_error(_BAD_OUT_OF_INDEX_ARRAY);
   }
 
   for (size_t i = 0; i < other_set.count; i++) {
+    if (i >= other_set.count)
+      throw std::overflow_error(_BAD_OUT_OF_INDEX_ARRAY);
     if (main_set.find(other_set.elements[i]) == main_set.end())
       return true;
   }
--- main.cxx
+++ main.cxx
@@ -1,6 +1,7 @@
 #include "lab4.cxx"
 #include "lab4.hxx"
 #include <iostream>
+#include <stdexcept>
 
 #define _UINPUT_ADD "\nВведите элемент который хотите добавить: "
 #define _UINPUT_SUB "\nВведите элемент который хотите удалить: "
@@ -10,7 +11,6 @@ using namespace std;
 int main(void) {
   set<uint32_t> setA, setB;
   uint32_t varik = 0, uinput = 0;
-  char setSelect = 0;
 
   while (true) {
     cout << "Выберите опцию:\n";
@@ -27,57 +27,62 @@ int main(void) {
     cout << "Ваше действие: ";
     cin >> varik;
 
-    switch (varik) {
-    case 1:
-      cout << _UINPUT_ADD;
-      cin >> uinput;
-      setA.add_elem(uinput);
-      break;
-    case 2:
-      cout << _UINPUT_ADD;
-      cin >> uinput;
-      setB.add_elem(uinput);
-      break;
-    case 3:
-      cout << _UINPUT_SUB;
-      cin >> uinput;
-      setA = setA - uinput;
-      break;
-    case 4:
-      cout << _UINPUT_SUB;
-      cin >> uinput;
-      setB = setB - uinput;
-      break;
-    case 5:
-      setA.print();
-      break;
-    case 6:
-      setB.print();
-      break;
-    case 7:
-      if (setB > setA)
-        cout << "Множество B является подмножеством A\n";
-      else
-        cout << "Множество B не является подмножеством A\n";
-      break;
-    case 8:
-      if (setA != setB)
-        cout << "Множества A и B неравны\n";
-      else
-        cout << "Множества A и B равны\n";
-      break;
-    case 9:
-      setA.clear();
-      setB.clear();
-      break;
+    try {
+      switch (varik) {
+      case 1:
+        cout << _UINPUT_ADD;
+        cin >> uinput;
+        setA.add_elem(uinput);
+        break;
+      case 2:
+        cout << _UINPUT_ADD;
+        cin >> uinput;
+        setB.add_elem(uinput);
+        break;
+      case 3:
+        cout << _UINPUT_SUB;
+        cin >> uinput;
+        setA = setA - uinput;
+        break;
+      case 4:
+        cout << _UINPUT_SUB;
+        cin >> uinput;
+        setB = setB - uinput;
+        break;
+      case 5:
+        setA.print();
+        break;
+      case 6:
+        setB.print();
+        break;
+      case 7:
+        if (setB > setA)
+          cout << "Множество B является подмножеством A\n";
+        else
+          cout << "Множество B не является подмножеством A\n";
+        break;
+      case 8:
+        if (setA != setB)
+          cout << "Множества A и B неравны\n";
+        else
+          cout << "Множества A и B равны\n";
+        break;
+      case 9:
+        setA.clear();
+        setB.clear();
+        break;
+      }
+
+      if (varik == 10)
+        break;
+
+      varik = 0;
+      uinput = 0;
+    } catch (const std::overflow_error &e) {
+      cerr << "Переполнение: " << e.what() << "\n";
+    } catch (const std::invalid_argument &e) {
+      cerr << "Передан неверный аргумент: " << e.what() << "\n";
     }
-
-    if (varik == 10)
-      break;
-
-    varik = 0;
-    uinput = 0;
-    setSelect = 0;
   }
 
   return 0;
